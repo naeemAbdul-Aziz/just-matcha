de52e117a181934dcc6e3269f27d10a47edfb05f
naeemaziz <naeemabdulaziz202@gmail.com>
2026-02-10 14:36:54 +0000

    Fix tailwind content paths

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/*.{js,ts,jsx,tsx,mdx}", // Explicitly catch files in app root
   ],
   theme: {
     extend: {
